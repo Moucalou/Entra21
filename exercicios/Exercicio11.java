3f0e3ea7069a47c92e5f86efe5c6eb29e7bfe551
Moucalou <moucalou@gmail.com>
2025-04-11 20:44:17 -0300

    Exercicio 11 - Preencher uma lista com 10 numeros

---
+++ exercicios/Exercicio11.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercicio11 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> lista = new ArrayList<>();
+		
+		lista.add(1);
+		lista.add(2);
+		lista.add(3);
+		lista.add(4);
+		lista.add(5);
+		lista.add(6);
+		lista.add(7);
+		lista.add(8);
+		lista.add(9);
+		lista.add(10);
+		
+		System.out.println(lista);
+		
+	}
+
+}
