3aa921c8317c372ba716a4e78d2c8859f4d869e5
Moucalou <moucalou@gmail.com>
2025-04-11 20:57:42 -0300

    Exercicio 23 - Media dos numeros pares da lista

---
+++ exercicios/Exercicio23.java
@@ -0,0 +1,33 @@
+package exercicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercicio23 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> lista = new ArrayList<>();
+		
+		int soma = 0, cont = 0;
+		double media;
+		
+		lista.add(1);
+		lista.add(2);
+		lista.add(3);
+		lista.add(4);
+		lista.add(6);
+		
+		for (int i = 0; i < lista.size(); i++) {
+			if (lista.get(i) % 2 == 0) {
+				soma += lista.get(i);
+				cont++;
+			}
+		}
+
+		media = (double) soma / cont;
+		
+		System.out.println(media);
+	}
+
+}
