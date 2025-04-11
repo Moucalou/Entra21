f8dc2ff24d22c6aa5b60a30734ccbe76fa8edfc8
Moucalou <moucalou@gmail.com>
2025-04-11 20:33:02 -0300

    Exercicio 1 - Contagem de 1 a 10

---
+++ exercicios/Exercicio1.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		//imprima os números de 1 a 10 usando um for
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println((i+1));
+		}
+
+	}
+
+}
