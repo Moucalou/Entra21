c13e46e33d5193ff747a518f66897a3b701a160d
Moucalou <moucalou@gmail.com>
2025-04-11 21:39:30 -0300

    Exercicio - Verificar se string é valida

---
+++ Desafios/Exercicio4.java
@@ -0,0 +1,33 @@
+package program;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		
+		List<Character> list = new ArrayList<>();
+		String s = "({[]})";
+	
+		for (int i = 0; i < s.length(); i ++) {
+			if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+				list.add(s.charAt(i));
+			} else if (s.charAt(i) == ')' && list.getLast() == '(') {
+				list.removeLast();
+			} else if (s.charAt(i) == ']' && list.getLast() == '[') {
+				list.removeLast();
+			} else if (s.charAt(i) == '}' && list.getLast() == '{') {
+				list.removeLast();
+			}
+			
+		}
+		
+		if (list.size() == 0) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+	}
+
+}
