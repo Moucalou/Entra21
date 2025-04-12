a5b40716148f898b9724cb92f8e4a6204cc8af95
Moucalou <moucalou@gmail.com>
2025-04-11 21:02:50 -0300

    Exercicio 30 - Contar digitos de um numero

---
+++ exercicios/Exercicio30.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio30 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um numero: ");
+		String numero = sc.nextLine();
+		
+		System.out.println("O numero de digitos é: " + numero.length());
+
+	}
+
+}
